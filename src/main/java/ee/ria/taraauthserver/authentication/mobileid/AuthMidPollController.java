72825f6b6453b4894e28cb8568bd0b51481f8d05
Marten Muru <marten.muru@nortal.com>
2021-02-03 02:02:37 +0200

    TARA2-105 - mid poll errors now return 500 or 502 when appropriate

--- src/main/java/ee/ria/taraauthserver/authentication/mobileid/AuthMidPollController.java
+++ src/main/java/ee/ria/taraauthserver/authentication/mobileid/AuthMidPollController.java
@@ -1,6 +1,8 @@
 package ee.ria.taraauthserver.authentication.mobileid;
 
+import ee.ria.taraauthserver.error.ErrorCode;
 import ee.ria.taraauthserver.error.exceptions.BadRequestException;
+import ee.ria.taraauthserver.error.exceptions.ServiceNotAvailableException;
 import ee.ria.taraauthserver.session.SessionUtils;
 import ee.ria.taraauthserver.session.TaraAuthenticationState;
 import ee.ria.taraauthserver.session.TaraSession;
@@ -30,9 +32,15 @@ public class AuthMidPollController {
 
         if (taraSession.getState() == NATURAL_PERSON_AUTHENTICATION_COMPLETED) {
             return new ModelAndView(new MappingJackson2JsonView(), Map.of("status", "COMPLETED"));
-        } else if (taraSession.getState() == AUTHENTICATION_FAILED)
-            throw new BadRequestException(taraSession.getAuthenticationResult().getErrorCode(), "Mid poll failed");
-        else
+        } else if (taraSession.getState() == AUTHENTICATION_FAILED) {
+            ErrorCode errorCode = taraSession.getAuthenticationResult().getErrorCode();
+            if (errorCode.equals(ErrorCode.ERROR_GENERAL))
+                throw new IllegalStateException(errorCode.getMessage());
+            else if (errorCode.equals(ErrorCode.MID_INTERNAL_ERROR))
+                throw new ServiceNotAvailableException(errorCode, "Sid poll failed", null);
+            else
+                throw new BadRequestException(taraSession.getAuthenticationResult().getErrorCode(), "Sid poll failed");
+        } else
             return new ModelAndView(new MappingJackson2JsonView(), Map.of("status", "PENDING"));
     }
 }
