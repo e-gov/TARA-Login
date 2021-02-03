cb0a96c0999b7f4b8a765bb81d3740f02d799c54
Marten Muru <marten.muru@nortal.com>
2021-02-03 02:35:32 +0200

    TARA2-105 - tests for mid poll AUTHENTICATION_FAILED

--- src/test/java/ee/ria/taraauthserver/authentication/mobileid/AuthMidPollControllerTest.java
+++ src/test/java/ee/ria/taraauthserver/authentication/mobileid/AuthMidPollControllerTest.java
@@ -1,16 +1,19 @@
 package ee.ria.taraauthserver.authentication.mobileid;
 
 import ee.ria.taraauthserver.BaseTest;
+import ee.ria.taraauthserver.error.ErrorCode;
 import ee.ria.taraauthserver.session.MockSessionFilter;
 import ee.ria.taraauthserver.session.TaraAuthenticationState;
 import ee.ria.taraauthserver.session.TaraSession;
 import io.restassured.RestAssured;
 import io.restassured.http.Cookie;
+import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.session.Session;
@@ -110,5 +113,61 @@ class AuthMidPollControllerTest extends BaseTest {
 
         TaraSession taraSession = sessionRepository.findById(sessionFilter.getSession().getId()).getAttribute(TARA_SESSION);
         assertEquals(NATURAL_PERSON_AUTHENTICATION_COMPLETED, taraSession.getState());
+    }
+
+    @Test
+    @Tag(value = "MID_AUTH_FAILED")
+    void midAuth_session_status_authentication_error_general() {
+        TaraSession.AuthenticationResult authenticationResult = new TaraSession.AuthenticationResult();
+        authenticationResult.setErrorCode(ErrorCode.ERROR_GENERAL);
+
+        MockSessionFilter sessionFilter = withTaraSession()
+                .sessionRepository(sessionRepository)
+                .authenticationTypes(of(MOBILE_ID))
+                .authenticationState(AUTHENTICATION_FAILED)
+                .authenticationResult(authenticationResult).build();
+        given()
+                .filter(sessionFilter)
+                .when()
+                .get("/auth/mid/poll")
+                .then()
+                .assertThat()
+                .statusCode(500)
+                .headers(EXPECTED_HTML_RESPONSE_HEADERS)
+                .body("error", equalTo("Internal Server Error"))
+                .body("message", equalTo("Autentimine ebaõnnestus teenuse tehnilise vea tõttu. Palun proovige mõne aja pärast uuesti."))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
+
+        TaraSession taraSession = sessionRepository.findById(sessionFilter.getSession().getId()).getAttribute(TARA_SESSION);
+        assertEquals(AUTHENTICATION_FAILED, taraSession.getState());
+        assertEquals(ErrorCode.ERROR_GENERAL, taraSession.getAuthenticationResult().getErrorCode());
+    }
+
+    @Test
+    @Tag(value = "MID_AUTH_FAILED")
+    void midAuth_session_status_authentication_mid_internal_error() {
+        TaraSession.AuthenticationResult authenticationResult = new TaraSession.AuthenticationResult();
+        authenticationResult.setErrorCode(ErrorCode.MID_INTERNAL_ERROR);
+
+        MockSessionFilter sessionFilter = withTaraSession()
+                .sessionRepository(sessionRepository)
+                .authenticationTypes(of(MOBILE_ID))
+                .authenticationState(AUTHENTICATION_FAILED)
+                .authenticationResult(authenticationResult).build();
+        given()
+                .filter(sessionFilter)
+                .when()
+                .get("/auth/mid/poll")
+                .then()
+                .assertThat()
+                .statusCode(502)
+                .headers(EXPECTED_HTML_RESPONSE_HEADERS)
+                .body("error", equalTo("Bad Gateway"))
+                .body("message", equalTo("Mobiil-ID teenuses esinevad tehnilised tõrked. Palun proovige mõne aja pärast uuesti."))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
+
+        TaraSession taraSession = sessionRepository.findById(sessionFilter.getSession().getId()).getAttribute(TARA_SESSION);
+        assertEquals(AUTHENTICATION_FAILED, taraSession.getState());
+        assertEquals(ErrorCode.MID_INTERNAL_ERROR, taraSession.getAuthenticationResult().getErrorCode());
     }
 }
