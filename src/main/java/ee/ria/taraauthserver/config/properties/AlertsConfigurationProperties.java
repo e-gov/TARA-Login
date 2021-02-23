ebc6d0e090a25e5e1d12f12feed01633d9733025
Marten Muru <marten.muru@nortal.com>
2021-02-23 04:35:57 +0200

    TARA2-120 - implemented alerts funtionality

---
+++ src/main/java/ee/ria/taraauthserver/config/properties/AlertsConfigurationProperties.java
@@ -0,0 +1,23 @@
+package ee.ria.taraauthserver.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@Validated
+@ConfigurationProperties(prefix = "tara.alerts")
+public class AlertsConfigurationProperties {
+
+    @NotNull
+    private String hostUrl;
+
+    private int connectionTimeoutMilliseconds = 3000;
+
+    private int readTimeoutMilliseconds = 3000;
+
+    private int refreshAlertsIntervalInMilliseconds = 300000;
+
+}
