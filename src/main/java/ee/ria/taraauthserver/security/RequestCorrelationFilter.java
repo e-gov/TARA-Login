8882a80a801dd4c367b625db549fe11d90526b2e
Alar Kvell <alar.kvell@nortal.com>
2022-06-17 18:02:18 +0300

    GSSO-119 Include build number in version

--- src/main/java/ee/ria/taraauthserver/security/RequestCorrelationFilter.java
+++ src/main/java/ee/ria/taraauthserver/security/RequestCorrelationFilter.java
@@ -1,10 +1,10 @@
 package ee.ria.taraauthserver.security;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@RequiredArgsConstructor
 public class RequestCorrelationFilter extends OncePerRequestFilter {
 
     private static final String MDC_ATTRIBUTE_KEY_VERSION = "service.version";
@@ -21,7 +20,12 @@ public class RequestCorrelationFilter extends OncePerRequestFilter {
     public static final String MDC_ATTRIBUTE_KEY_REQUEST_TRACE_ID = "trace.id";
     private static final String REQUEST_ATTRIBUTE_NAME_REQUEST_ID = "requestId";
 
-    private final BuildProperties buildProperties;
+    private final String version;
+
+    public RequestCorrelationFilter(BuildProperties buildProperties, GitProperties gitProperties) {
+        version = getVersion(buildProperties, gitProperties);
+        logger.info("Application version: " + version);
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -36,8 +40,10 @@ public class RequestCorrelationFilter extends OncePerRequestFilter {
         // NB! Set traceId also as HttpServletRequest attribute to make it accessible for Tomcat's AccessLogValve
         request.setAttribute(REQUEST_ATTRIBUTE_NAME_REQUEST_ID, requestTraceId);
 
-        if (buildProperties != null) {
-            MDC.put(MDC_ATTRIBUTE_KEY_VERSION, buildProperties.getVersion());
+        if (version != null) {
+            MDC.put(MDC_ATTRIBUTE_KEY_VERSION, version);
+        } else {
+            MDC.remove(MDC_ATTRIBUTE_KEY_VERSION);
         }
 
         String ipAddress = request.getRemoteAddr();
@@ -55,4 +61,20 @@ public class RequestCorrelationFilter extends OncePerRequestFilter {
         //  As the second-best solution, we are (re)setting _all_ values above at the start of request. Usually request
         //  threads and background threads are not cross-used.
     }
+
+    private String getVersion(BuildProperties buildProperties, GitProperties gitProperties) {
+        if (buildProperties != null) {
+            String versionWithoutBuildNumber = buildProperties.getVersion();
+
+            if (gitProperties != null) {
+                String buildNumber = gitProperties.get("build.number");
+                if (StringUtils.isNotEmpty(buildNumber)) {
+                    return versionWithoutBuildNumber + "-" + buildNumber;
+                }
+            }
+            return versionWithoutBuildNumber;
+        }
+        return null;
+    }
+
 }
