72c9dc5cf70daeb11688e37fdd4a725befb30483
Alar Kvell <alar.kvell@nortal.com>
2022-06-19 14:27:09 +0300

    GSSO-114 Homogenize logging configuration

--- src/main/java/ee/ria/taraauthserver/logging/LogbackJsonFactoryDecorator.java
+++ src/main/java/ee/ria/taraauthserver/logging/LogbackJsonFactoryDecorator.java
@@ -3,7 +3,9 @@ package ee.ria.taraauthserver.logging;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import net.logstash.logback.decorate.JsonFactoryDecorator;
 
 public class LogbackJsonFactoryDecorator implements JsonFactoryDecorator {
@@ -12,9 +14,10 @@ public class LogbackJsonFactoryDecorator implements JsonFactoryDecorator {
     public JsonFactory decorate(JsonFactory factory) {
         ObjectMapper objectMapper = (ObjectMapper) factory.getCodec();
         objectMapper
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .setDateFormat(new StdDateFormat().withColonInTimeZone(false))
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         return factory;
     }
 }
