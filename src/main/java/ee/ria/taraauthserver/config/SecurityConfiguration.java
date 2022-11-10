d498d8fe0a82399d5c77a8c762bc7d739be876f1
Juku Trump <juku.trump@nortal.com>
2022-11-10 13:24:02 +0200

    AUT-1055 Use Web eID for authentication; remove old authentication logic; use POST request for /auth/id/init endpoint; remove warnings related to outdated ID card software

--- src/main/java/ee/ria/taraauthserver/config/SecurityConfiguration.java
+++ src/main/java/ee/ria/taraauthserver/config/SecurityConfiguration.java
@@ -5,20 +5,13 @@ import ee.ria.taraauthserver.error.ErrorCode;
 import ee.ria.taraauthserver.error.exceptions.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.CsrfException;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -31,112 +24,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static ee.ria.taraauthserver.authentication.idcard.IdCardController.AUTH_ID_REQUEST_MAPPING;
-
 @Slf4j
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     public static final String TARA_SESSION_CSRF_TOKEN = "tara.csrf";
+    private final AuthConfigurationProperties authConfigurationProperties;
 
-    @Order(1)
-    @ConditionalOnProperty(value = "tara.auth-methods.id-card.basic-auth.enabled")
-    @Configuration
-    @RequiredArgsConstructor
-    // TODO Replace deprecated WebSecurityConfigurerAdapter with SecurityFilterChain etc.
-    public static class IdCardApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-        private final AuthConfigurationProperties authConfigurationProperties;
-        @Value("${tara.auth-methods.id-card.basic-auth.username}")
-        private String username;
-        @Value("${tara.auth-methods.id-card.basic-auth.password}")
-        private String password;
-
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .antMatcher(AUTH_ID_REQUEST_MAPPING)
-                    .exceptionHandling()
-                    .accessDeniedHandler(new CustomAccessDeniedHandler())
-                    .and()
-                    .securityContext().disable()
-                    .anonymous().disable()
-                    .logout().disable()
-                    .rememberMe().disable()
-                    .servletApi().disable()
-                    .sessionManagement().disable()
-                    .authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                    .httpBasic()
-                    .and()
-                    .headers()
-                    .xssProtection().xssProtectionEnabled(false)
-                    .and()
-                    .frameOptions().deny()
-                    .contentSecurityPolicy(authConfigurationProperties.getContentSecurityPolicy())
-                    .and()
-                    .httpStrictTransportSecurity()
-                    .includeSubDomains(true)
-                    .maxAgeInSeconds(16070400);
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth
-                    .inMemoryAuthentication()
-                    .withUser(username)
-                    .password(passwordEncoder().encode(password))
-                    .roles("AUTH_ID_REQUEST");
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .securityContext().disable()
+                .anonymous().disable()
+                .logout().disable()
+                .rememberMe().disable()
+                .httpBasic().disable()
+                .servletApi().disable()
+                .sessionManagement().disable()
+                .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository()))
+                .headers()
+                .xssProtection().xssProtectionEnabled(false)
+                .and()
+                .frameOptions().deny()
+                .contentSecurityPolicy(authConfigurationProperties.getContentSecurityPolicy())
+                .and()
+                .httpStrictTransportSecurity()
+                .includeSubDomains(true)
+                .maxAgeInSeconds(16070400);
+        return http.build();
     }
 
-    @Order(2)
-    @Configuration
-    @RequiredArgsConstructor
-    public static class AuthenticationApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-        private final AuthConfigurationProperties authConfigurationProperties;
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .exceptionHandling()
-                    .accessDeniedHandler(new CustomAccessDeniedHandler())
-                    .and()
-                    .securityContext().disable()
-                    .anonymous().disable()
-                    .logout().disable()
-                    .rememberMe().disable()
-                    .httpBasic().disable()
-                    .servletApi().disable()
-                    .sessionManagement().disable()
-                    .csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository()))
-                    .headers()
-                    .xssProtection().xssProtectionEnabled(false)
-                    .and()
-                    .frameOptions().deny()
-                    .contentSecurityPolicy(authConfigurationProperties.getContentSecurityPolicy())
-                    .and()
-                    .httpStrictTransportSecurity()
-                    .includeSubDomains(true)
-                    .maxAgeInSeconds(16070400);
-        }
-
-        @Override
-        public void configure(WebSecurity webSecurity) {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> {
             StrictHttpFirewall firewall = new StrictHttpFirewall();
             firewall.setUnsafeAllowAnyHttpMethod(true);
-            webSecurity.httpFirewall(firewall);
-        }
+            web.httpFirewall(firewall);
+        };
+    }
 
-        @Bean
-        public CsrfTokenRepository csrfTokenRepository() {
-            HttpSessionCsrfTokenRepository tokenRepository = new HttpSessionCsrfTokenRepository();
-            tokenRepository.setSessionAttributeName(TARA_SESSION_CSRF_TOKEN);
-            return tokenRepository;
-        }
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository tokenRepository = new HttpSessionCsrfTokenRepository();
+        tokenRepository.setSessionAttributeName(TARA_SESSION_CSRF_TOKEN);
+        return tokenRepository;
     }
 
     static class CustomAccessDeniedHandler implements AccessDeniedHandler {
