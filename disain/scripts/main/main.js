2e36c046ef41fc7bf7d01b2262fc556625fa2ec0
Alar Kvell <alar.kvell@nortal.com>
2023-06-29 15:29:13 +0300

    AUT-505 Hide ID-card authentication method on iOS devices

--- disain/scripts/main/main.js
+++ disain/scripts/main/main.js
@@ -534,6 +534,6 @@ jQuery(function ($) {
     }
 
 	function isProbablyMobileDevice() {
-		return /Mobi|Android/i.test(navigator.userAgent);
+		return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 	}
 });
