0ad351a66f38c64bb20d1789280902ba031d31e9
Rene Kütt <Rene.Kytt@nortal.com>
2023-09-18 13:44:03 +0300

    AUT-1435 Fix unit test

--- src/test/java/ee/ria/taraauthserver/authentication/idcard/IdCardErrorControllerTest.java
+++ src/test/java/ee/ria/taraauthserver/authentication/idcard/IdCardErrorControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.session.SessionRepository;
 import static ch.qos.logback.classic.Level.ERROR;
 import static ch.qos.logback.classic.Level.WARN;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.endsWithIgnoringCase;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.hamcrest.Matchers.nullValue;
@@ -127,7 +127,7 @@ class IdCardErrorControllerTest extends BaseTest {
                 .assertThat()
                 .statusCode(400)
                 .body("message", startsWith("ID-kaardiga veebis sisselogimiseks ja allkirjastamiseks vajalik<span translate=\"no\"> Web eID </span>laiendus ei ole Teie arvutis töökorras.<br>Palun kontrollige vastavalt <a href=\"https://www.id.ee/artikkel/id-kaardiga-sisenemine-voi-allkirjastamine-e-teenustes-ebaonnestub-2/\">id.ee juhendile</a>, kas ID-tarkvara on ajakohane ja veebilehitseja õigesti seadistatud.<br>Uuendamata ID-tarkvaraga ja valesti seadistatud veebilehitsejaga ei ole võimalik ID-kaardiga sisse logida "))
-                .body("message", endsWith(" Autentimisteenuse kaudu.<br>Vea kood<span translate=\"no\"> ERR_WEBEID_UNKNOWN_ERROR</span>"))
+                .body("message", endsWithIgnoringCase(" Autentimisteenuse kaudu.<br>Vea kood<span translate=\"no\"> ERR_WEBEID_UNKNOWN_ERROR</span>"))
                 .body("incident_nr", matchesPattern("[a-f0-9]{32}"))
                 .body("reportable", equalTo(true));
 
