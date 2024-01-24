9ef28bad65954e8d2967f894ba99d373d55afb89
Marten Muru <marten.muru@nortal.com>
2024-01-24 11:24:40 +0200

    AUT-1564 Update configuration for Spring Security 6

--- src/main/java/ee/ria/taraauthserver/config/SecurityConfiguration.java
+++ src/main/java/ee/ria/taraauthserver/config/SecurityConfiguration.java
@@ -12,17 +12,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.csrf.CsrfException;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
@@ -32,6 +37,7 @@ import java.io.IOException;
 import static ee.ria.taraauthserver.authentication.eidas.EidasCallbackController.EIDAS_CALLBACK_REQUEST_MAPPING;
 
 @Slf4j
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
@@ -40,34 +46,41 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .exceptionHandling()
-                .accessDeniedHandler(new CustomAccessDeniedHandler())
-                .and()
-                .securityContext().disable()
-                .anonymous().disable()
-                .logout().disable()
-                .rememberMe().disable()
-                .httpBasic().disable()
-                .servletApi().disable()
-                .sessionManagement().disable()
+                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
+                .securityContext(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .rememberMe(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .servletApi(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
                 .csrf(this::configureCsrf)
-                .headers()
-                .xssProtection().xssProtectionEnabled(false)
-                .and()
-                .frameOptions().deny()
-                .contentSecurityPolicy(authConfigurationProperties.getContentSecurityPolicy())
-                .and()
-                .httpStrictTransportSecurity()
-                .includeSubDomains(true)
-                .maxAgeInSeconds(16070400);
+                .headers(headersConfigurer -> headersConfigurer
+                        .xssProtection(xssConfig -> xssConfig
+                                .headerValue(XXssProtectionHeaderWriter.HeaderValue.DISABLED))
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                        .contentSecurityPolicy(policyConfig -> policyConfig
+                                .policyDirectives(authConfigurationProperties.getContentSecurityPolicy()))
+                        .httpStrictTransportSecurity(hstsConfig -> hstsConfig
+                                .includeSubDomains(true)
+                                .maxAgeInSeconds(16070400)));
         return http.build();
     }
 
     private void configureCsrf(CsrfConfigurer<HttpSecurity> csrf) {
         csrf.csrfTokenRepository(csrfTokenRepository())
+                .csrfTokenRequestHandler(csrfRequestHandler())
                 .requireCsrfProtectionMatcher(new AndRequestMatcher(
                         CsrfFilter.DEFAULT_CSRF_MATCHER,
                         new NegatedRequestMatcher(new AntPathRequestMatcher(EIDAS_CALLBACK_REQUEST_MAPPING))));
+    }
+
+    private CsrfTokenRequestAttributeHandler csrfRequestHandler() {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        //Opt-out of Deferred CSRF Tokens as described in https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#deferred-csrf-token
+        requestHandler.setCsrfRequestAttributeName(null);
+        return requestHandler;
     }
 
     @Bean
