ab4cb1555d3aaea944c0f0b266180f50990bd6a8
Marten Muru <marten.muru@nortal.com>
2024-01-24 11:27:20 +0200

    AUT-1564 Update HttpClient configurations for HttpClient5

--- src/main/java/ee/ria/taraauthserver/config/AlertsConfiguration.java
+++ src/main/java/ee/ria/taraauthserver/config/AlertsConfiguration.java
@@ -4,8 +4,13 @@ import ee.ria.taraauthserver.config.properties.AlertsConfigurationProperties;
 import ee.ria.taraauthserver.logging.ClientRequestLogger.Service;
 import ee.ria.taraauthserver.logging.RestTemplateErrorLogger;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -25,17 +30,29 @@ public class AlertsConfiguration {
 
     @Bean
     public RestTemplate alertsRestTemplate(RestTemplateBuilder builder, SSLContext sslContext, AlertsConfigurationProperties alertsConfigurationProperties) {
+        @SuppressWarnings("resource")
         HttpClient client = HttpClients.custom()
-                .setSSLContext(sslContext)
-                .setMaxConnPerRoute(3)
-                .setMaxConnTotal(3)
+                .setConnectionManager(createConnectionManager(sslContext, alertsConfigurationProperties))
                 .build();
 
+        //The setReadTimeout() method of this builder is not usable because we are instantiating our own HttpComponentsClientHttpRequestFactory, which does not support it.
         return builder
-                .setConnectTimeout(Duration.ofSeconds(alertsConfigurationProperties.getConnectionTimeoutMilliseconds()))
-                .setReadTimeout(Duration.ofSeconds(alertsConfigurationProperties.getReadTimeoutMilliseconds()))
+                .setConnectTimeout(Duration.ofMillis(alertsConfigurationProperties.getConnectionTimeoutMilliseconds()))
                 .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(client))
                 .errorHandler(new RestTemplateErrorLogger(Service.ALERTS))
+                .build();
+    }
+
+    private static HttpClientConnectionManager createConnectionManager(SSLContext sslContext, AlertsConfigurationProperties alertsConfigurationProperties) {
+        SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(Timeout.ofMilliseconds(alertsConfigurationProperties.getReadTimeoutMilliseconds())).build();
+
+        return PoolingHttpClientConnectionManagerBuilder.create()
+                .setDefaultSocketConfig(socketConfig)
+                .setMaxConnPerRoute(3)
+                .setMaxConnTotal(3)
+                .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
+                        .setSslContext(sslContext)
+                        .build())
                 .build();
     }
 }
