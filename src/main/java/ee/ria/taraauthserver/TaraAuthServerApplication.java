a2239b008ee128769c5d578e071abe158eaf5ad8
Marten Muru <marten.muru@nortal.com>
2024-07-04 09:15:56 +0300

    AUT-1641 Use jar packaging

--- src/main/java/ee/ria/taraauthserver/TaraAuthServerApplication.java
+++ src/main/java/ee/ria/taraauthserver/TaraAuthServerApplication.java
@@ -9,17 +9,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
-public class TaraAuthServerApplication extends SpringBootServletInitializer {
+public class TaraAuthServerApplication {
 
     public static void main(String[] args) {
         ElasticApmAttacher.attach();
         SpringApplication.run(TaraAuthServerApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        ElasticApmAttacher.attach();
-        builder.application().addListeners(new ActiveConfigurationLogger());
-        return builder.sources(TaraAuthServerApplication.class);
     }
 }
