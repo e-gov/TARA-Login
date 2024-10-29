8169f7ece2103c701f4efda7be2a8154d08fbecb
Alvar Sokk <alvar.sokk@nortal.com>
2024-10-29 17:21:17 +0200

    AUT-1628 Improving session timeout modal

--- disain/scripts/timeout/timeout-modal.js
+++ disain/scripts/timeout/timeout-modal.js
@@ -5,24 +5,30 @@ jQuery(function ($) {
     addLoginAuthFlowTimeout();
 
     function addLoginAuthFlowTimeout() {
-        let secondsToTimeout = $('#auth-flow-timeout-timer').get(0).getAttribute('data-seconds-to-timeout');
-        if (secondsToTimeout == null) {
+        const timeoutElement = $('#auth-flow-timeout-timer').get(0);
+        if (!timeoutElement) {
             return null;
         }
-        secondsToTimeout = parseInt(secondsToTimeout,10);
+
+        const secondsToTimeout = parseInt(
+            timeoutElement.getAttribute('data-seconds-to-timeout'), 10);
+        if (isNaN(secondsToTimeout)) {
+            return null;
+        }
 
         let currentTimestamp = getCurrentTimeStampInSeconds();
         timeoutTimestamp = currentTimestamp + secondsToTimeout;
 
         const timeouts = [
-            {threshold: 60},   //1 minute
-            {threshold: 300},  //5 minutes
+            { threshold: 60 },   //1 minute
+            { threshold: 300 },  //5 minutes
         ];
 
-        timeouts.forEach(({threshold}) => {
+        timeouts.forEach(({ threshold }) => {
             const timeDifference = secondsToTimeout - threshold;
             if (timeDifference > 0) {
-                setTimeout(() => displayTimeoutModal(threshold), timeDifference * 1000);
+                setTimeout(() => displayTimeoutModal(threshold),
+                    timeDifference * 1000);
             }
         });
 
@@ -30,17 +36,18 @@ jQuery(function ($) {
         setTimeout(throwTimeoutError, secondsToTimeout * 1000);
     }
 
-    function throwTimeoutError(){
-        if($('#id-card-wait').hasClass('hidden')){
+    function throwTimeoutError() {
+        if ($('#id-card-wait').hasClass('hidden')) {
             window.location.href = '/error-handler?error_code=auth_flow_timeout';
-        }else{
+        } else {
             setTimeout(throwTimeoutError, 1000); //extend session 1 second and try again
         }
     }
 
-    function displayTimeoutModal( secondsToTimeout){
-        let authFlowTimeout = document.getElementById('auth-flow-timeout');
-        if(authFlowTimeout.classList.contains('show') || !$('#id-card-wait').hasClass('hidden')){
+    function displayTimeoutModal(secondsToTimeout) {
+        let authFlowTimeout = $('#auth-flow-timeout').get(0);
+        if (authFlowTimeout.classList.contains('show')
+            || !$('#id-card-wait').hasClass('hidden')) {
             return null;
         }
 
@@ -49,9 +56,12 @@ jQuery(function ($) {
         authFlowTimeout.classList.add('show');
         authFlowTimeout.setAttribute('aria-hidden', 'false');
         authFlowTimeout.focus();
+        $(document).on('keydown', handleEscapeKey);
+        $(document).on('keydown', handleFocusTrap);
 
-        let intervalId = setInterval(function() {
-            if (!authFlowTimeout.classList.contains('show') || secondsToTimeout<=0) {
+        let intervalId = setInterval(function () {
+            if (!authFlowTimeout.classList.contains('show') || secondsToTimeout
+                <= 0) {
                 clearInterval(intervalId);
             }
             incrementAuthFlowTimeoutTimer(secondsToTimeout);
@@ -59,22 +69,37 @@ jQuery(function ($) {
         }, 1000);
     }
 
-    function incrementAuthFlowTimeoutTimer(secondsToTimeout){
+    function incrementAuthFlowTimeoutTimer(secondsToTimeout) {
         let minutes = String(Math.floor(secondsToTimeout / 60));
         let seconds = String(secondsToTimeout % 60).padStart(2, '0');
 
         $('#auth-flow-timeout-timer').text(`${minutes}:${seconds}`);
-        if (seconds==='00') {
-            $('#auth-flow-timeout-minute-counter').text(minutes);
-        }
     }
 
-    function addListeners(){
+    function addListeners() {
         $('.modal .close,.modal .accept').click(hideModal);
     }
 
-    function hideModal(){
-        $(this).closest('.modal').removeClass('show').attr('aria-hidden', 'true');
+    function hideModal() {
+        $('.modal').removeClass('show').attr('aria-hidden', 'true');
+        $(document).off('keydown', handleEscapeKey);
+        $(document).off('keydown', handleFocusTrap);
+    }
+
+    function handleEscapeKey(event) {
+        if (event.key === 'Escape' || event.key === 'Esc') {
+            hideModal();
+        }
+    }
+
+    function handleFocusTrap(event) {
+        const focusButton = $('#auth-flow-timeout .accept');
+
+        focusButton.focus();
+
+        if (event.key === 'Tab' || event.key === 'Shift+Tab') {
+            event.preventDefault();
+        }
     }
 
     function getCurrentTimeStampInSeconds() {
