ae79c342af266c3480bb24b1e47384766a628c26
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2025-10-16 15:31:15 +0300

    AUT-2332 Fix error view rendering failing

--- src/main/java/ee/ria/taraauthserver/config/thymeleaf/PrettyDatesExpression.java
+++ src/main/java/ee/ria/taraauthserver/config/thymeleaf/PrettyDatesExpression.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
+import java.util.Date;
 import java.util.Locale;
 
 @RequiredArgsConstructor
@@ -19,16 +22,32 @@ public class PrettyDatesExpression {
 
     private final MessageSource messageSource;
 
+    public String dateTime(Date dateTime) {
+        return getFormatter(DATETIME_FORMAT_KEY).format(toZonedDateTime(dateTime));
+    }
+
     public String dateTime(TemporalAccessor dateTime) {
         return getFormatter(DATETIME_FORMAT_KEY).format(dateTime);
     }
 
-    public String dateText(TemporalAccessor dateText) {
-        return getFormatter(DATE_TEXT_FORMAT_KEY).format(dateText);
+    public String dateText(Date date) {
+        return dateText(toZonedDateTime(date));
+    }
+
+    public String dateText(TemporalAccessor date) {
+        return getFormatter(DATE_TEXT_FORMAT_KEY).format(date);
+    }
+
+    public String date(Date date) {
+        return date(toZonedDateTime(date));
     }
 
     public String date(TemporalAccessor date) {
         return getFormatter(DATE_FORMAT_KEY).format(date);
+    }
+
+    private ZonedDateTime toZonedDateTime(Date date) {
+        return ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
     public DateTimeFormatter getFormatter(String code) {
