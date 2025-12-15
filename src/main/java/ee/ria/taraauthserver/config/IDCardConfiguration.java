25d6f1d18bc657d0cfe7f6f0b3e559db50870d9c
Madis Jaagup Laurson <madisjaagup.laurson@nortal.com>
2025-12-15 11:18:45 +0200

    AUT-2381 Use ResilientOcspRevocationChecker for OCSP requests

--- src/main/java/ee/ria/taraauthserver/config/IDCardConfiguration.java
+++ src/main/java/ee/ria/taraauthserver/config/IDCardConfiguration.java
@@ -2,12 +2,23 @@ package ee.ria.taraauthserver.config;
 
 import ee.ria.taraauthserver.config.properties.AuthConfigurationProperties;
 import ee.ria.taraauthserver.utils.X509Utils;
+import eu.webeid.security.ResilientOcspRevocationChecker;
+import eu.webeid.security.certificate.CertificateValidator;
 import eu.webeid.security.challenge.ChallengeNonceGenerator;
 import eu.webeid.security.challenge.ChallengeNonceGeneratorBuilder;
 import eu.webeid.security.challenge.ChallengeNonceStore;
 import eu.webeid.security.exceptions.JceException;
+import eu.webeid.security.exceptions.OCSPCertificateException;
 import eu.webeid.security.validator.AuthTokenValidator;
 import eu.webeid.security.validator.AuthTokenValidatorBuilder;
+import eu.webeid.security.validator.ocsp.OcspClient;
+import eu.webeid.security.validator.ocsp.OcspClientImpl;
+import eu.webeid.security.validator.ocsp.OcspServiceProvider;
+import eu.webeid.security.validator.ocsp.service.AiaOcspServiceConfiguration;
+import eu.webeid.security.validator.ocsp.service.FallbackOcspServiceConfiguration;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.RetryConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +27,18 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.KeyStore;
+import java.security.cert.CertStore;
 import java.security.cert.PKIXParameters;
 import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static ee.ria.taraauthserver.config.properties.AuthConfigurationProperties.IdCardAuthConfigurationProperties;
 import static java.util.stream.Collectors.toMap;
@@ -75,19 +91,118 @@ public class IDCardConfiguration {
     }
 
     @Bean
-    public AuthTokenValidator validator(AuthConfigurationProperties configurationProvider, Map<String, X509Certificate> trustedCertificatesMap) {
+    public AuthTokenValidator validator(AuthConfigurationProperties authConfigurationProperties,
+                                        IdCardAuthConfigurationProperties idCardAuthConfigurationProperties,
+                                        Map<String, X509Certificate> trustedCertificatesMap) {
         X509Certificate[] certificates = trustedCertificatesMap.values().toArray(new X509Certificate[0]);
+
+        Duration ocspRequestTimeout = Duration.ofSeconds(5);
+
+        // These are the defaults of CircuitBreakerConfig.
+        int slidingWindowSize = 100;
+        int minimumNumberOfCalls = 100;
+        int failureRateThreshold = 50;
+        int permittedNumberOfCallsInHalfOpenState = 10;
+        Duration waitDurationInOpenState = Duration.ofSeconds(60);
+
+        Duration allowedOcspResponseTimeSkew = Duration.ofMinutes(15);
+        // TODO There should be two separate values here.
+        Duration maxOcspResponseThisUpdateAge = Duration.ofMinutes(2);
+        boolean rejectUnknownOcspResponseStatus = true;
+
         try {
-            return new AuthTokenValidatorBuilder()
-                    .withSiteOrigin(configurationProvider.getSiteOrigin().toURI())
-                    // TARA is using customized OCSP validation instead of AuthTokenValidator's built-in check
-                    .withoutUserCertificateRevocationCheckWithOcsp()
-                    // This duplicates some checks that have already been implemented in OCSPValidator class,
-                    // but we still need to set the trusted CA certificates here.
-                    .withTrustedCertificateAuthorities(certificates)
+            AiaOcspServiceConfiguration aiaOcspServiceConfiguration
+                    = getAiaOcspServiceConfiguration(idCardAuthConfigurationProperties, trustedCertificatesMap);
+
+            List<FallbackOcspServiceConfiguration> fallbackOcspServiceConfigurations
+                    = getFallbackOcspServiceConfigurations(idCardAuthConfigurationProperties, trustedCertificatesMap);
+
+            OcspServiceProvider ocspServiceProvider = new OcspServiceProvider(
+                    null,
+                    aiaOcspServiceConfiguration,
+                    fallbackOcspServiceConfigurations
+            );
+
+            OcspClient ocspClient = OcspClientImpl.build(ocspRequestTimeout);
+
+            CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                    .slidingWindowSize(slidingWindowSize)
+                    .minimumNumberOfCalls(minimumNumberOfCalls)
+                    .failureRateThreshold(failureRateThreshold)
+                    .permittedNumberOfCallsInHalfOpenState(permittedNumberOfCallsInHalfOpenState)
+                    .waitIntervalFunctionInOpenState(IntervalFunction.of(waitDurationInOpenState))
                     .build();
-        } catch (JceException | URISyntaxException e) {
+
+            RetryConfig retryConfig = RetryConfig.ofDefaults();
+
+            ResilientOcspRevocationChecker ocspRevocationChecker = new ResilientOcspRevocationChecker(
+                    ocspClient,
+                    ocspServiceProvider,
+                    circuitBreakerConfig,
+                    retryConfig,
+                    allowedOcspResponseTimeSkew,
+                    maxOcspResponseThisUpdateAge,
+                    rejectUnknownOcspResponseStatus
+            );
+
+            return new AuthTokenValidatorBuilder()
+                    .withSiteOrigin(authConfigurationProperties.getSiteOrigin().toURI())
+                    .withTrustedCertificateAuthorities(certificates)
+                    .withOcspCertificateRevocationChecker(ocspRevocationChecker)
+                    .build();
+        } catch (JceException | URISyntaxException | OCSPCertificateException e) {
             throw new RuntimeException("Error building the Web eID auth token validator.", e);
         }
+    }
+
+    private static AiaOcspServiceConfiguration getAiaOcspServiceConfiguration(IdCardAuthConfigurationProperties idCardAuthConfigurationProperties,
+                                                                             Map<String, X509Certificate> trustedCertificatesMap) throws JceException {
+        List<URI> nonceDisabledOcspUrls = idCardAuthConfigurationProperties.getOcsp().stream()
+                .filter(AuthConfigurationProperties.Ocsp::isNonceDisabled)
+                .map(ocsp -> URI.create(ocsp.getUrl()))
+                .toList();
+
+        Set<TrustAnchor> trustedCACertificateAnchors = CertificateValidator
+                .buildTrustAnchorsFromCertificates(trustedCertificatesMap.values());
+        CertStore trustedCACertificateCertStore = CertificateValidator
+                .buildCertStoreFromCertificates(trustedCertificatesMap.values());
+        return new AiaOcspServiceConfiguration(
+                nonceDisabledOcspUrls,
+                trustedCACertificateAnchors,
+                trustedCACertificateCertStore
+        );
+    }
+
+    private static List<FallbackOcspServiceConfiguration> getFallbackOcspServiceConfigurations(IdCardAuthConfigurationProperties idCardAuthConfigurationProperties,
+                                                       Map<String, X509Certificate> trustedCertificatesMap) throws OCSPCertificateException {
+        List<FallbackOcspServiceConfiguration> fallbackOcspServiceConfigurations = new ArrayList<>();
+
+        for (AuthConfigurationProperties.Ocsp fallback : idCardAuthConfigurationProperties.getFallbackOcsp()) {
+            if (fallback.getResponderCertificateCn() == null) {
+                // TODO Throw an exception
+                continue;
+            }
+            for (String issuerCn : fallback.getIssuerCn()) {
+                AuthConfigurationProperties.Ocsp primary = idCardAuthConfigurationProperties.getOcsp().stream()
+                        .filter(ocsp -> ocsp.getIssuerCn().contains(issuerCn))
+                        // TODO What if there are multiple entries?
+                        .findFirst()
+                        .orElse(null);
+                if (primary == null) {
+                    // TODO Throw an exception
+                    continue;
+                }
+                X509Certificate responderCertificate = trustedCertificatesMap.get(fallback.getResponderCertificateCn());
+                FallbackOcspServiceConfiguration fallbackOcspServiceConfiguration = new FallbackOcspServiceConfiguration(
+                        URI.create(primary.getUrl()),
+                        URI.create(fallback.getUrl()),
+                        responderCertificate,
+                        !fallback.isNonceDisabled()
+                );
+                log.info("Created a fallback configuration. Primary URL: {}, fallback URL: {}, does support nonce: {}", fallbackOcspServiceConfiguration.getOcspServiceAccessLocation(), fallbackOcspServiceConfiguration.getFallbackOcspServiceAccessLocation(), fallbackOcspServiceConfiguration.doesSupportNonce());
+                fallbackOcspServiceConfigurations.add(fallbackOcspServiceConfiguration);
+            }
+        }
+        return fallbackOcspServiceConfigurations;
     }
 }
