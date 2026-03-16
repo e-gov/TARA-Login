5200040965b63b2d883210abe3ef56ddaf8bd917
Markus Tamm <markus.tamm@nortal.com>
2026-03-16 13:45:43 +0200

    AUT-2607 Add Smart-ID headers to polling views

--- disain/scripts/form/sid-web2app-poll.js
+++ disain/scripts/form/sid-web2app-poll.js
@@ -11,7 +11,7 @@
         // setTimeout() timers required for polling.
         e.preventDefault();
         initAuthenticationAndStartPolling();
-        hide(".c-layout--full > .container");
+        hide("#tab-form-content");
         hide(".link-back-mobile");
         show("#smart-id-web2app-wait");
     });
