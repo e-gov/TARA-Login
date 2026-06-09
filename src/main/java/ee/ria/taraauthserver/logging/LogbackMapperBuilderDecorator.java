98bfbb075fa6e694b3d70937c2f08f4b5ec01f44
Sander Kondratjev <sander.kondratjev@nortal.com>
2026-06-09 13:33:24 +0300

    AUT-2753 Improve decorator

--- src/main/java/ee/ria/taraauthserver/logging/LogbackMapperBuilderDecorator.java
+++ src/main/java/ee/ria/taraauthserver/logging/LogbackMapperBuilderDecorator.java
@@ -4,16 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import net.logstash.logback.decorate.MapperBuilderDecorator;
 import tools.jackson.databind.cfg.DateTimeFeature;
 import tools.jackson.databind.PropertyNamingStrategies;
-import tools.jackson.databind.SerializationFeature;
-import tools.jackson.databind.cfg.MapperBuilder;
+import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.util.StdDateFormat;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
-public class LogbackMapperBuilderDecorator implements MapperBuilderDecorator {
+public class LogbackMapperBuilderDecorator implements MapperBuilderDecorator<JsonMapper, JsonMapper.Builder> {
 
     @Override
-    public Object decorate(Object decoratable) {
-        MapperBuilder builder = (MapperBuilder) decoratable;
+    public JsonMapper.Builder decorate(JsonMapper.Builder builder) {
         builder.configure(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         builder.defaultDateFormat(new StdDateFormat().withColonInTimeZone(false));
         builder.changeDefaultPropertyInclusion(inclusion -> JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL));
