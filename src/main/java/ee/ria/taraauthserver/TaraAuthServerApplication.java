6b8608e218c8e81305cebcafb207b4cd972df307
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2026-06-17 14:20:32 +0300

    AUT-2909 Remove unused class `ActiveConfigurationLogger`

--- src/main/java/ee/ria/taraauthserver/TaraAuthServerApplication.java
+++ src/main/java/ee/ria/taraauthserver/TaraAuthServerApplication.java
@@ -1,7 +1,6 @@
 package ee.ria.taraauthserver;
 
 import co.elastic.apm.attach.ElasticApmAttacher;
-import ee.ria.taraauthserver.logging.ActiveConfigurationLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
